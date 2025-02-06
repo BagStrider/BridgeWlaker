a839061431b747fc3bf30956556f020fe7bc5d50
Andrey Kanuhnevich <andrey64931@gmail.com>
2025-02-06 15:36:34 +0300

    PointsCounter

---
+++ Assets/Scripts/Points/PointsCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PointsCounter
+{
+    public event Action<int> OnChanged;
+
+    private int _points;
+
+    public void AddPoints(int value)
+    {
+        _points += value;
+
+        OnChanged?.Invoke(_points);
+    }
+}
---
+++ Assets/Scripts/Points/PointsCounterView.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PointsCounterView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _pointsText;
+
+    public void ChangePoints(int points)
+    {
+        _pointsText.text = points.ToString();
+    }
+}
